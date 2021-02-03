85aa696e611e3fde4904f6730af22f4f30eac23a
Denniel Stephen Mordeno <dennielstephen@gmail.com>
2021-02-03 22:32:37 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RoomProvider } from "./context";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <RoomProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RoomProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
